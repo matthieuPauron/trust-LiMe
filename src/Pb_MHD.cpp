9bc41d464d426fc640b1f696b9373591b9b8ee6f
Matthieu Pauron - Dell Precision <matthieu.pauron.pro@gmail.com>
2026-02-05 20:51:57 +0100

    Progress on Pb_MHD

--- src/Pb_MHD.cpp
+++ src/Pb_MHD.cpp
@@ -11,7 +11,46 @@
 
 Implemente_instanciable(Pb_MHD, "Pb_MHD", Pb_Thermohydraulique);
 
-Sortie& Pb_
+Sortie& Pb_MHD::printOn(Sortie& os) const { return Pb_Thermohydraulique::printOn(os); }
+Entree& Pb_MHD::readOn(Entree& is) { return Pb_Thermohydraulique::readOn(is); }
+
+
+const Equation_base& Pb_MHD::equation(int i) const
+{
+  if (i == 0) return eq_hydraulique;
+  else if (i == 1) return eq_thermique;
+  else if (i == 2) return eq_magnetic_field;
+  else
+    {
+      Cerr << "Pb_Thermohydraulique::equation() : Wrong equation number" << i << "!" << finl;
+      Process::exit();
+    }
+  return eq_hydraulique;
+}
+
+Equation_base& Pb_MHD::equation(int i) const
+{
+  if (i == 0) return eq_hydraulique;
+  else if (i == 1) return eq_thermique;
+  else if (i == 2) return eq_magnetic_field;
+  else
+    {
+      Cerr << "Pb_Thermohydraulique::equation() : Wrong equation number" << i << "!" << finl;
+      Process::exit();
+    }
+  return eq_hydraulique;
+}
+
+void Pb_MHD::associer_milieu_base(const Milieu_base& mil)
+{
+  Pb_Hydraulique::associer_milieu_base(mil);
+  eq_magnetic_field.associer_milieu_base(mil);
+}
+
+int Pb_MHD::verifier()
+{
+  return 1;
+}
 
 
 
